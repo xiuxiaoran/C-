8e5e5f200b64e9b38144e3430339de1a5b84ef8b
xiuxiaoran <ymm19970920@163.com>
2017-11-12 17:43:50 +0800

    杭电2001基础题
    计算两点间的距离

---
+++ 2001.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	double x1,x2,y1,y2;
+	double distance;
+	while(scanf("%lf%lf%lf%lf",&x1,&y1,&x2,&y2)!=EOF)
+	{
+	
+		distance=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		printf("%.2lf\n",distance);		
+	}
+}
+	
+
